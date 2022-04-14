347a176a7cdc66be99275a9bcda7620c0477a854
Belyaev-MP <m.belyaev.sstu@gmail.com>
2022-04-14 15:43:59 +0400

    Initial commit

---
+++ src/main/java/my/self/springapp/SpringWebAppIfbsApplication.java
@@ -0,0 +1,13 @@
+package my.self.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppIfbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppIfbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/self/springapp/SpringWebAppIfbsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.self.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppIfbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
